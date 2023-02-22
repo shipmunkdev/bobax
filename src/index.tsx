a467812a54771ace508c55d2f378d8dff7a010a4
Sein Tun <sein@mbp.local>
2023-02-21 21:56:30 -0800

    Install and configure prettier for react and TS

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Heading from './components/header/Heading';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Heading />
+  </React.StrictMode>,
 );
