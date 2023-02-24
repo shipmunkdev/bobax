d43f2d52dce905845a424aebf2ff7784edb9614e
Sein Tun <sein@mbp.local>
2023-02-24 13:54:04 -0800

    Added Navbar with Homepage template. Installed bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Heading from './components/header/Heading';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Homepage from './pages/Home';
+import NavigationBar from './components/NavigationBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Heading />
+    <NavigationBar />
+    <Homepage />
   </React.StrictMode>,
 );
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Homepage = () => {
+  return (
+    <Container>
+      <h1>Boba Drinks Card Content</h1>
+    </Container>
+  );
+};
+
+export default Homepage;
