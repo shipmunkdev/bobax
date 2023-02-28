ac701ffd63b3758f3438ee1dc9503f3e78a13264
Simon <simon.tan09@gmail.com>
2023-02-27 22:28:50 -0800

    update list to 5 drinks

--- src/components/BobaContainer/bobalist.jsx
+++ src/components/BobaContainer/bobalist.jsx
@@ -9,6 +9,21 @@ const bobalist = [
     price: 6,
     description: 'A Coffeed , Duh!',
   },
+  {
+    name: 'Pink Drink',
+    price: 6,
+    description: 'A strawbeery starbuck inspired drink',
+  },
+  {
+    name: 'Brown sugar',
+    price: 6,
+    description: 'very sweet , brown sugar',
+  },
+  {
+    name: 'water',
+    price: 3,
+    description: 'Organic water',
+  },
 ];
 
 export default bobalist
