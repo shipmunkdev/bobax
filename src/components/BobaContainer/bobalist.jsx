36c77dc416f3bc429cbb2c74374c2060bd743e8e
Simon <simon.tan09@gmail.com>
2023-02-27 19:42:34 -0800

    add scoll bar

--- src/components/BobaContainer/bobalist.jsx
+++ src/components/BobaContainer/bobalist.jsx
@@ -1,13 +1,14 @@
-const bobalist =
-[
-    {
-        'name': 'Matcha Latte',
-        'price': 5,
-        'description':"It's A matcha with milk "
-    },
-    {
-        'name': 'Coffee',
-        'price': 6,
-        'description':"It's a Coffeed , Duh! "
-    }
-]
+const bobalist = [
+  {
+    name: 'Matcha Latte',
+    price: 5,
+    description: 'A matcha Latte',
+  },
+  {
+    name: 'Coffee',
+    price: 6,
+    description: 'A Coffeed , Duh!',
+  },
+];
+
+export default bobalist
--- src/components/BobaContainer/index.jsx
+++ src/components/BobaContainer/index.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
-import bobalist from './bobalist.jsx'
+import bobalist from './bobalist.jsx';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import './index.css'
 
-const BobaContainer = () =>{
-    return (
-        <div>
-            <div>
-                {bobalist.map((boba) => (<div key={boba.name}>
-                    <div>{boba.name}</div>
-                    <div>{boba.price}</div>
-                    <div>{boba.description}</div>
-                </div>))}
-            </div>
-        </div>
-    )
-}
+const BobaContainer = () => {
+  return (
+    <div className='indBoba'>
+        {bobalist.map((boba) => (
+            <Card key={boba.name} style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>{boba.name}</Card.Title>
+                    <Card.Text>
+                        <div>{boba.description}</div>
+                        <div>${boba.price}</div>
+                    </Card.Text>
+                    <Button variant="primary">Add to cart</Button>
+                </Card.Body>
+          </Card>
+        ))}
+    </div>
+  );
+};
 
-export default BobaContainer
+export default BobaContainer;
