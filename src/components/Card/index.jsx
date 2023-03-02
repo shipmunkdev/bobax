1d627d8f6c20418ce58a9ca383a27b85130bcaaa
Simon <simon.tan09@gmail.com>
2023-03-02 00:03:43 -0800

    try modal from react-bootstrap

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+// import React, {useState} from 'react';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col'
 import Buttonx from '../Button/index.jsx';
 import PropTypes from 'prop-types';
+// import Modalx from './cardmodal.jsx'
+
 import './index.css'
+
 const IndCard = ({ boba }) => {
-  return (
-    <Col className='boba_col' md='auto' key={boba.name}>
+
+    // const [item, setItem] = useState ('')
+    // const [show, setShow] = useState (false)
+
+    // const addtocart = async(e) => {
+    //     e.preventDefault();
+    //     //add data here ?
+    // }
+
+    // const togglemodal = () => {setShow(true)}
+
+    return (
+    <Col className='boba_col' md='auto' key={boba.name}
+    // onClick={togglemodal}
+    >
         <Card key={boba.name} variant='m-3' style={{ width: '18rem' }}>
         <Card.Img variant='top' src='holder.js/100px180' />
         <Card.Body>
@@ -18,6 +34,7 @@ const IndCard = ({ boba }) => {
             <Buttonx variant={'primary'} text={'Add to cart'} />
         </Card.Body>
         </Card>
+        {/* <Modalx show={show} /> */}
     </Col>
   );
 };
