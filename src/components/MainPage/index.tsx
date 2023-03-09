09226d9cebcd96136dfedb2527cbe74460426621
Simon <simon.tan09@gmail.com>
2023-03-08 18:38:05 -0800

    make nav and home page a sibiling

---
+++ src/components/MainPage/index.tsx
@@ -0,0 +1,13 @@
+import NavigationBar from '../NavigationBar'
+import Homepage from '../../pages/Home'
+
+const MainPage = () => {
+    return (
+        <>
+            <NavigationBar />
+            <Homepage />
+        </>
+    )
+}
+
+export default MainPage
