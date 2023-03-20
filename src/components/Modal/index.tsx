4f882b7d78dc40e5c8535ce0926488d9a40d7e2a
Simon <simon.tan09@gmail.com>
2023-03-19 17:37:07 -0700

    Revert "checkbox and radiobutton and its relate setstate working"
    This reverts commit c9a3694ea91c0fbcfa173d052b603d70ce94147a.

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -4,12 +4,10 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import { ModalProps } from 'types/common/main';
-import Form from 'react-bootstrap/Form';
+import React from 'react';
 
 function BobaCustomizeableModal(props: ModalProps) {
-  const { bobaInfoModal, modalShow, milk, setMilk, toppings, addTopping, onHide } = props;
-  const { imageLink, description } = bobaInfoModal;
-
+  const { bobaInfoModal, modalShow, onHide } = props;
   return (
     <Modal
       show={modalShow}
@@ -21,94 +19,28 @@ function BobaCustomizeableModal(props: ModalProps) {
         <Modal.Title id='contained-modal-title-vcenter'>{bobaInfoModal?.id}</Modal.Title>
       </Modal.Header>
       <Modal.Body className='show-grid'>
-        <img style={{ width: '24rem' }} src={imageLink}></img>
-        <p>{description}</p>
-        <Form>
-          <Container>
-            <Row>
-              <Col xs={12} md={8}>
-                Quantity
-              </Col>
-              <Col xs={6} md={4}>
-                number
-              </Col>
-            </Row>
-
+        <Container>
+          <Row>
             <Col xs={12} md={8}>
-              Milk(Required)
-              <Form.Group>
-                <Form.Check
-                  type={'radio'}
-                  id={'default-radio'}
-                  label={'Organic Whole Milk'}
-                  value={'Organic Whole Milk'}
-                  onChange={(e) => {
-                    setMilk(e.target.value);
-                  }}
-                  checked={milk === 'Organic Whole Milk'}
-                />
-                <Form.Check
-                  type={'radio'}
-                  id={'default-radio'}
-                  label={'Oat Milk'}
-                  value={'Oat Milk'}
-                  onChange={(e) => {
-                    setMilk(e.target.value);
-                  }}
-                  checked={milk === 'Oat Milk'}
-                />
-                <Form.Check
-                  type={'radio'}
-                  id={'default-radio'}
-                  label={'Almond Milk'}
-                  value={'Almond Milk'}
-                  onChange={(e) => {
-                    setMilk(e.target.value);
-                  }}
-                  checked={milk === 'Almond Milk'}
-                />
-              </Form.Group>
+              .col-xs-12 .col-md-8
             </Col>
-            <Row>
-              <Col xs={12} md={8}>
-                Toppings(Optional)
-                <Form.Check
-                  type={'checkbox'}
-                  id={'default-checkbox'}
-                  label={'Boba($0.75)'}
-                  value={'Boba'}
-                  onChange={(e) => {
-                    addTopping(e.target.value, e.target.checked);
-                  }}
-                  checked={toppings.some((t) => t.name === 'Boba')}
-                />
-                <Form.Check
-                  type={'checkbox'}
-                  id={'default-checkbox'}
-                  label={'Mango Jelly($0.75)'}
-                  value={'Mango Jelly'}
-                  onChange={(e) => {
-                    addTopping(e.target.value, e.target.checked);
-                  }}
-                  checked={toppings.some((t) => t.name === 'Mango Jelly')}
-                />
-                <Form.Check
-                  type={'checkbox'}
-                  id={'default-checkbox'}
-                  label={'Coconut Pudding($0.75)'}
-                  value={'Coconut Pudding'}
-                  onChange={(e) => {
-                    addTopping(e.target.value, e.target.checked);
-                  }}
-                  checked={toppings.some((t) => t.name === 'Coconut Pudding')}
-                />
-              </Col>
-              <Col xs={6} md={4}>
-                Toggle Options(not done)
-              </Col>
-            </Row>
-          </Container>
-        </Form>
+            <Col xs={6} md={4}>
+              .col-xs-6 .col-md-4
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs={6} md={4}>
+              .col-xs-6 .col-md-4
+            </Col>
+            <Col xs={6} md={4}>
+              .col-xs-6 .col-md-4
+            </Col>
+            <Col xs={6} md={4}>
+              .col-xs-6 .col-md-4
+            </Col>
+          </Row>
+        </Container>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={onHide}>Close</Button>
