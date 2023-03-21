6ec3f0f7da02c653ec7f9f9f79075587a1624fbb
Sein Tun <contact@seintun.dev>
2023-03-21 13:59:14 -0700

    Tests enhancements

--- src/components/Modal/index.test.tsx
+++ src/components/Modal/index.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import BaseModal from 'components/Modal';
 
@@ -8,11 +8,13 @@ describe('BaseModal', () => {
     const handleClose = jest.fn();
     const modalShow = true;
 
-    const { getByText } = render(
+    render(
       <BaseModal title='Test Modal Title' modalShow={modalShow} onHide={handleClose} />,
     );
-
+    
+    const { getByText } = within(screen.getByTestId('modal-title'));
     expect(getByText('Test Modal Title')).toBeInTheDocument();
+    expect(screen.getByTestId('modal-title')).toBeInTheDocument();
   });
 
   it('should render close button', () => {
@@ -23,6 +25,7 @@ describe('BaseModal', () => {
 
     const closeButton = screen.getByTestId('modal-close-button');
     expect(closeButton).toBeInTheDocument();
+    expect(closeButton).toHaveTextContent('Close');
   });
 
   it('should call onHide when close button is clicked', () => {
@@ -34,6 +37,7 @@ describe('BaseModal', () => {
     const closeButton = screen.getByTestId('modal-close-button');
     userEvent.click(closeButton);
     expect(handleClose).toHaveBeenCalled();
+    expect(handleClose).toHaveBeenCalledTimes(1);
   });
 
   it('should render Test Modal Body when modalBody props is passed', () => {
@@ -49,7 +53,9 @@ describe('BaseModal', () => {
       />,
     );
 
-    const testModalBody = screen.getByTestId('modal-body');
-    expect(testModalBody.textContent).toBe('Test Modal Body');
+    const { getByText } = within(screen.getByTestId('modal-body'));
+    expect(getByText('Test Modal Body')).toBeInTheDocument();
+    expect(screen.getByTestId('modal-body')).toBeInTheDocument();
+    expect(screen.getByTestId('modal-body')).toHaveClass('show-grid');
   });
 });
