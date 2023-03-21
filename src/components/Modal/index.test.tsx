676162120cc42e68266232e4fc8ddfef9b0a308b
Sein Tun <contact@seintun.dev>
2023-03-21 13:51:47 -0700

    Cleanup comments

--- src/components/Modal/index.test.tsx
+++ src/components/Modal/index.test.tsx
@@ -5,11 +5,9 @@ import BaseModal from 'components/Modal';
 
 describe('BaseModal', () => {
   it('renders with title when opened', () => {
-    // Arrange
     const handleClose = jest.fn();
     const modalShow = true;
 
-    // Act
     const { getByText } = render(
       <BaseModal title='Test Modal Title' modalShow={modalShow} onHide={handleClose} />,
     );
@@ -18,38 +16,30 @@ describe('BaseModal', () => {
   });
 
   it('should render close button', () => {
-    // Arrange
     const handleClose = jest.fn();
     const modalShow = true;
 
-    // Act
     render(<BaseModal title='Test Modal Title' modalShow={modalShow} onHide={handleClose} />);
 
-    // Assert
     const closeButton = screen.getByTestId('modal-close-button');
     expect(closeButton).toBeInTheDocument();
   });
 
   it('should call onHide when close button is clicked', () => {
-    // Arrange
     const handleClose = jest.fn();
     const modalShow = true;
 
-    // Act
     render(<BaseModal title='Test Modal' modalShow={modalShow} onHide={handleClose} />);
 
-    // Assert
     const closeButton = screen.getByTestId('modal-close-button');
     userEvent.click(closeButton);
     expect(handleClose).toHaveBeenCalled();
   });
 
   it('should render Test Modal Body when modalBody props is passed', () => {
-    // Arrange
     const handleClose = jest.fn();
     const modalShow = true;
 
-    // Act
     render(
       <BaseModal
         title='Test Modal'
@@ -59,7 +49,6 @@ describe('BaseModal', () => {
       />,
     );
 
-    // Assert
     const testModalBody = screen.getByTestId('modal-body');
     expect(testModalBody.textContent).toBe('Test Modal Body');
   });
