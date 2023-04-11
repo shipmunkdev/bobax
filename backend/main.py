f6ebbf774184ee6deab044302eed797713655d64
Simon <simon.tan09@gmail.com>
2023-04-10 20:33:15 -0700

    changing relative path file for database

--- backend/main.py
+++ backend/main.py
@@ -10,19 +10,19 @@ async def root():
 
 @app.get('/boba_list')
 async def get_boba_list():
-  with open('../database/bobalist.json') as f:
+  with open('./database/bobalist.json') as f:
       # Load the JSON data into a Python object
     bobaList = json.load(f)
   return bobaList["bobaList"]
 
 @app.get('/milk_option')
 async def get_milk_option():
-  with open('../database/milkoption.json') as f:
+  with open('./database/milkoption.json') as f:
     milkOption = json.load(f)
   return milkOption["milkOption"]
 
 @app.get('/toppings_option')
 async def get_milk_option():
-  with open('../database/toppingsoption.json') as f:
+  with open('./database/toppingsoption.json') as f:
     toppingsOption = json.load(f)
   return toppingsOption["toppingsList"]
