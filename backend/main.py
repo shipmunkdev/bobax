fe0f96e407426df9bee9e56e03f974f2e89959b0
Simon M Tan <103973779+SimonMTan@users.noreply.github.com>
2023-04-10 18:30:10 -0700

    [BOBAX-43]make boba list and have Getroute (#25)
    * make boba list and have Getroute
    
    * restructure folder
    
    * adding database file
    
    * restructure the getcall inbackend
    
    * delete unnecessary folder

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 
+import json
+
 app = FastAPI()
 
 @app.get('/')
 async def root():
   return {"message": "Hello World"}
 
-
+@app.get('/boba_list')
+async def get_boba_list():
+with open('../database/bobalist.json') as f:
+    # Load the JSON data into a Python object
+    data = json.load(f)
+  return data
