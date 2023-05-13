e74a27105ce7e61073de2bfb49579540f91f126d
Simon <simon.tan09@gmail.com>
2023-05-13 15:23:28 -0700

    fix graphQL query for milk in the backend

--- backend/MilkList/query.py
+++ backend/MilkList/query.py
@@ -2,37 +2,15 @@ import graphene
 import json
 
 class MilkOptionType(graphene.ObjectType):
+    id = graphene.UUID()
     name = graphene.String()
 
-# helper function
-def create_milk_fields():
-    with open('./database/data.json') as f:
-        milkjson = json.load(f)['milkData']
-
-    fields = {}
-    for i, (key, value) in enumerate(milkjson.items(), start=1):
-        field_name = f"milk_{i}"
-        field = graphene.Field(MilkOptionType)
-        fields[field_name] = field
-
-    return type('MilkKeyType', (graphene.ObjectType,), fields)
-
-# since calling inside take care of the object Type as it is already define in helper funciton
-class MilkKeyType(create_milk_fields()):
-    pass
-
 class MilkQuery(graphene.ObjectType):
-    List = graphene.Field(MilkKeyType)
+    List = graphene.List(MilkOptionType)
 
     def resolve_List(self, info):
         with open('./database/data.json') as f:
             milkjson = json.load(f)['milkData']
-
-        # dynamically creating in query
-        milk_values = {}
-        for key, value in milkjson.items():
-            milk_values[key] = MilkOptionType(name=value['name'])
-
-        return MilkKeyType(**milk_values)
-
+        return milkjson
+        
 schemaMilk = graphene.Schema(query=MilkQuery)
