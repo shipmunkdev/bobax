b6a8af78755fb4bc64324e491b22a47792b16c49
simon <simon.tan09@gmail.com>
2023-08-14 13:55:03 -0700

    second attempt debug CORS issue

--- backend/main.py
+++ backend/main.py
@@ -11,11 +11,11 @@ app = FastAPI()
 
 # this is to allow CORS so that you can connect backend and frontend locally
 load_dotenv()
-# origins = os.environ.get("ALLOW_CORS", "").split(",")
+origins = os.environ.get("ALLOW_CORS", "").split(",")
 
 app.add_middleware(
   CORSMiddleware,
-  allow_origins=["https://stbobax.netlify.app"],
+  allow_origins=["https://stbobax.netlify.app","https://backend_bobax-1-f0092940.deta.app",],
   allow_credentials=True,
   allow_methods=["*"],
   allow_headers=["*"],
